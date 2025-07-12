2def4ce6963bf3c545e70b24a64d99eb4c6df857
Gogashe <gogashekmsk.02@gmail.com>
2025-07-12 12:01:19 +0200

    ndk 27.xxx + firebase dependensies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.bitrix_go"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13599879"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
